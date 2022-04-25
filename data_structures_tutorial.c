8a0b6ddd8d9dab553c824a5ec5af7a0928b0c8f7
Hüseyin SOYLU <huseyinsoylu@mail.ru>
2022-04-25 20:11:42 +0300

    Prepared: the file has been prepared for the first use

---
+++ data_structures_tutorial.c
@@ -0,0 +1,99 @@
+/*
+Huseyin Soylu - 171418012
+Marmara Universitesi Bilgisayar Muhendisligi Bolumu
+BLM2002-Veri Yapilari ve Algoritmalar Dersi - Donem Projesi
+
+Asagidaki program veri yapıları dersinde öğretilen temel veri türlerinin anlaşılması ve pratikte nasıl kullanıldıkları 
+hakkında kullanıcıya bilgi kazandırmayı hedefler.
+
+Kodlama yapilirken macar notasyonu esas alinmis ve tum degiskenler icin asagida 
+belirtilen esaslarda kodlama yapilmistir. 
+_____________________________________________________________________________________
+
+------For Variable-----------------
+variable = v; tip sonrasi eki
+-------------------------------
+
+------Use Case-----------------
+integer icin = iv;
+char icin =cv;
+double icin = dv;
+float icin = fv;
+-------------------------------
+_____________________________________________________________________________________
+
+------For Pointer------------------
+pointer = p; tip sonrasi eki
+-------------------------------
+
+------Use Case-----------------
+int pointer icin = ip;
+char pointer icin = cp;
+-------------------------------
+_____________________________________________________________________________________
+
+------ For Array---------------
+array = a; tip sonrasi eki
+-------------------------------
+
+------Use Case-----------------
+int array = ia;
+char array = ca;
+-------------------------------
+_____________________________________________________________________________________
+
+------For Function-------------
+function = f; tip sonrasi eki
+-------------------------------
+
+------Use Case-----------------
+int geri donuslu fonksiyon = if;
+char geri donuslu fonksiyon = cf;
+
+int pointer geri donuslu fonksiyon = ipf;
+char pointer geri donuslu fonksiyon = cpf;
+
+geri donussuz fonksiyon icin = vf;
+-------------------------------
+_____________________________________________________________________________________
+
+------For Function Parameter---
+parameter = p;
+-------------------------------
+
+------Use Case-----------------
+int geri donuslu parametreli fonsiyon = ifp;
+int pointer donuslu parametreli fonksiyon = ipfp;
+
+Seklinde tanimlanmistir.
+-------------------------------
+________________________________________________________________________________________
+*/
+//--------------------------------------Program Area--------------------------------------
+
+//--------------------------------------Program Libraries---------------------------------
+#include<stdio.h>
+
+//___________________________________________________________________________________
+
+
+
+
+//-------------------------------------Global Variables------------------------------
+
+//___________________________________________________________________________________
+
+
+
+
+//----------------------------------Function Declare Area----------------------------
+
+//___________________________________________________________________________________
+
+
+
+int main(int argc, char * argv[])
+{
+
+
+}
